9deebb0e2d1574b9bc392d9596220bdc6f17a156
Garrett M. <84481668+Garrett-M-code@users.noreply.github.com>
2026-05-22 21:59:13 -0400

    Added menubar

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ b.start()
  .set({ characters: '░▒░ ░██░> ████▒ >█░░ ██', speed: 100 })
  .reveal(4000); // Reveals the text over 4 second
 
+document.getElementById("about-btn").addEventListener("click", function() {
+	// Redirect to Portfolio Portion
+    window.location.href = "#about";   
+});  
+ 
+document.getElementById("members-btn").addEventListener("click", function() {
+	// Redirect to Portfolio Portion
+    window.location.href = "missing.html";   
+});  
+ 
+document.getElementById("research-btn").addEventListener("click", function() {
+	// Redirect to Portfolio Portion
+    window.location.href = "missing.html";   
+});  
+ 
+document.getElementById("contact-btn").addEventListener("click", function() {
+	// Redirect to Portfolio Portion
+    window.location.href = "#contact";   
+});   
