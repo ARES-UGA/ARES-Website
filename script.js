f73f33a5a3edad14b1fcc4e18779c4cdbf3618ba
Garrett M. <84481668+Garrett-M-code@users.noreply.github.com>
2026-05-22 21:20:41 -0400

    Added basic text animation

---
+++ script.js
@@ -0,0 +1,6 @@
+// Text "generation"
+const b = baffle('.landing-accent');
+b.start()
+ .set({ characters: '░▒░ ░██░> ████▒ >█░░ ██', speed: 100 })
+ .reveal(4000); // Reveals the text over 4 second
+
